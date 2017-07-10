dfbb31f6681eafc858beec2a58a499d888c66abe
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 16:14:49 +0000

    prepping for bootstrap

--- AppName/app/controllers/application_controller.rb
+++ AppName/app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+  
+  before_action :set_copyright
+  
+  def set_copyright
+    @copyright = YimViewTool::Renderer.copyright 'Richard Yim', 'All rights reserved'
+  end
 end
+
