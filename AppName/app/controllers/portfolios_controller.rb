f32ed9273325fa5e9743f01cc3ab9eba216de797
Jordan Hudgens <jordan@devcamp.com>
2017-07-25 15:58:09 +0000

    Implemented persistant position updates for drag and drop interface

--- AppName/app/controllers/portfolios_controller.rb
+++ AppName/app/controllers/portfolios_controller.rb
@@ -1,10 +1,18 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
-  
+
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
+  end
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
   end
 
   def angular
@@ -29,13 +37,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    
-
   end
 
   def update
-    
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -49,9 +53,6 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    # Perform the lookup
-    
-
     # Destroy/delete the record
     @portfolio_item.destroy
 
--- AppName/app/models/portfolio.rb
+++ AppName/app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
-  accepts_nested_attributes_for :technologies,
+  accepts_nested_attributes_for :technologies, 
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   include Placeholder
@@ -17,5 +17,10 @@ class Portfolio < ApplicationRecord
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
+
+
+  def self.by_position
+    order("position ASC")
   end
 end
