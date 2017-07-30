b43dfc4f323181b318071225b40ea0c1fc7291ad
Richard <richard@Heidis-MacBook-Pro.local>
2017-07-30 13:14:29 -0600

    Implemented twitter call on tech news page

--- AppName/app/controllers/pages_controller.rb
+++ AppName/app/controllers/pages_controller.rb
@@ -1,12 +1,16 @@
 class PagesController < ApplicationController
-  def home
-    @posts = Blog.all
-    @skills = Skill.all
-  end
+  	def home
+		@posts = Blog.all
+		@skills = Skill.all
+  	end
 
-  def about
-  end
+  	def about
+  	end
 
-  def contact
-  end
+  	def contact
+  	end
+
+  	def tech_news
+  		@tweets = SocialTool.twitter_search
+  	end
 end
